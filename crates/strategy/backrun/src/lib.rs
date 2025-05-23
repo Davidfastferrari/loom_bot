d3cdeaae029385bcf5a9de696faf745643b68af2
Davidfastferrari <daudahassan9825@gmail.com>
2025-05-23 02:23:29 +0100

    star

--- crates/strategy/backrun/src/lib.rs
+++ crates/strategy/backrun/src/lib.rs
@@ -5,11 +5,13 @@ pub use capital_manager::CapitalManager;
 pub use pending_tx_state_change_processor::PendingTxStateChangeProcessorActor;
 pub use state_change_arb_searcher::StateChangeArbSearcherActor;
 pub use swap_calculator::SwapCalculator;
+pub use profit_calculator::{ProfitCalculator, MultiCurrencyProfit};
 
 mod block_state_change_processor;
 mod capital_manager;
 mod pending_tx_state_change_processor;
 mod state_change_arb_searcher;
+mod profit_calculator;
 
 mod affected_pools_code;
 mod affected_pools_logs;
