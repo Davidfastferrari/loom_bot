5c824666089b3badb950fc198ca649251085fbd9
Davidfastferrari <daudahassan9825@gmail.com>
2025-05-27 21:47:40 +0100

    loom build

--- start_looms.sh
+++ start_looms.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Start loom_backrun in background
+# Start loom_backrun in background with full path
 /app/loom_backrun &
 
-# Start loom_base in background
+# Start loom_base in background with full path
 /app/loom_base &
 
 # Wait indefinitely to keep container running
