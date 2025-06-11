d05c4ccbce2de96fa13527f7a1b5c198f2ff12ca
Davidfastferrari <daudahassan9825@gmail.com>
2025-06-11 23:10:32 +0100

    now

--- crates/core/topology/src/topology.rs
+++ crates/core/topology/src/topology.rs
@@ -100,7 +100,7 @@ impl<
         self,
         swap_encoder: NE,
     ) -> Topology<DB, NE, P, Ethereum, LoomDataTypesEthereum> {
-        //let swap_encoder = Arc::new(swap_encoder);
+        let swap_encoder = Arc::new(swap_encoder);
         Topology {
             config: self.config,
             clients: self.clients,
