9ed480ab9491891afd0a1fb77f33e208a16908f2
Davidfastferrari <daudahassan9825@gmail.com>
2025-06-12 09:59:50 +0100

    timey

--- crates/core/topology/src/lib.rs
+++ crates/core/topology/src/lib.rs
@@ -4,7 +4,6 @@ pub use rate_limited_provider::RateLimitedProvider;
 
 mod topology;
 mod topology_config;
-pub use topology_config::ws_config;
 mod dns_config;
 pub use dns_config::configure_dns_settings;
 mod rate_limited_provider;
