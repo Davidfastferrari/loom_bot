11331138ec2e7b54c42e3892811245d8881dec58
Davidfastferrari <daudahassan9825@gmail.com>
2025-07-04 04:43:20 +0100

    day and night

--- crates/core/blockchain-actors/src/actor.rs
+++ crates/core/blockchain-actors/src/actor.rs
@@ -198,8 +198,8 @@ where
         // Run the preload_market_state async function synchronously
         let rt = Runtime::new()?;
         rt.block_on(async {
-        self.actor_manager.wait().await
-    }
+            self.actor_manager.wait().await
+        })
 
     /// Start a custom actor
     pub fn start<F>(&mut self, actor_factory: F) -> Result<&mut Self>
