14bb10b5b6263d504ad4037d47878c3e2c524b8f
Davidfastferrari <daudahassan9825@gmail.com>
2025-07-05 04:18:14 +0100

    kingdom

--- crates/strategy/utils/constants.rs
+++ crates/strategy/utils/constants.rs
@@ -1,5 +1,5 @@
-use alloy_primitives::Address;
 use lazy_static::lazy_static;
+use alloy_primitives::Address;
 
 lazy_static! {
     pub static ref COINBASE: Address = "0x1f9090aaE28b8a3dCeaDf281B0F12828e676c326".parse().unwrap();
